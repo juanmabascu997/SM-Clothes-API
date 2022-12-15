79c8d117b6a9fc207e3e00646dd7b977c27eb32b
Juan Manuel Bascuñan <70663191+juanmabascu997@users.noreply.github.com>
2022-12-15 19:31:48 -0300

    Update inventarioController.js

--- src/controller/inventarioController.js
+++ src/controller/inventarioController.js
@@ -24,7 +24,7 @@ const SendEmail = async (req) => {
 
 const sendInventario = async (req, res) => {
     try {
-        let response = await SendEmail(req.thisBody)
+        let response = await SendEmail(req.body.thisBody)
         res.send(response)
     } catch (err) {
         console.log(err)
