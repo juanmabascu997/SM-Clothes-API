5827c7bd416688948459fee95f52e8f21813416e
Juan Manuel Bascuñan <70663191+juanmabascu997@users.noreply.github.com>
2023-05-27 13:57:26 -0300

    Test

--- src/controller/productsController.js
+++ src/controller/productsController.js
@@ -49,17 +49,14 @@ const getProductosAndToken = async (token) => {
         let primeraproducts = await axios(getStockProducts);
         return primeraproducts
     } catch (err) {
-        // if(err.response.status === 401) {
-        //     let newToken = await axios(config).then(function (response) {
-        //         console.log(response);
-        //         return JSON.stringify(response.data.access_token);
-        //     });
-        //     getProductosAndToken(newToken);
-        // } else {
-        //     return false;
-        // }
-        console.log(err);
-        return false
+        if(err.response.statusCode === 401) {
+            let newToken = await axios(config).then(function (response) {
+                return JSON.stringify(response.data.access_token);
+            });
+            getProductosAndToken(newToken);
+        } else {
+            return false;
+        }
     }
 };
 
@@ -70,41 +67,41 @@ const getProducts = async (req, res) => {
     await sheet.clearRows()
 
     let token = 'qf-h2yn_Y0tgZ4NEWDHi62VdoOT-8ZmqVx-lt3Kd6bstnWavOXa2GhujTZSvGHrwmJJbt1TWitqV8dOOurujoDhIhi_GONzQVHkkUe7hpV9SMP_XImCS3jympISn4PdkpeNDY6FC_bsBVSY7tZqajOGUCigUx6Jro9V0kUMyXcxVVZ7zIIWNGv3zhexha374XmBLBrRzWycRaC-pbMQAXSgrbVYWgCTSMBbLiw510ycHgdgBbnh7yF6WbWDABOtacBWJVdMfgHEKqHYwfxQcPGXLHtvHEJhrmO1QDSmXNLu3TbES5T9JcWTtkW82Ub3rep5IQ2S9R_tuYPgc0gw8vNIysgcEZQbH55_4DPZ9UaEA217vWqEYOPg9cfT5OYeQZ4z7fouPsfy81F4O7LIVHwuWZbEqi0gQmB2jNZGbcm1M0XIGMy00XHUOzZLrC5whUHTUWjmt6hKXm95vZH3wUaU-4IzXYf3Jc9OoWqY1PCsBk5ujdwo_3WACrrxK3J554JN6vRK7iyFzXxauGr-1w8hnhNt77fGO7i9dxBMCoIVNaeNCDilNmzsOFVn1lJDnCDGvy3CDo2ZpBgDrgh0DnA'
-    if (doc) {
+    let primeraproducts = await getProductosAndToken(token)
+    if (doc && primeraproducts) {
     //   const token = await getToken();
-        let primeraproducts = await getProductosAndToken(token)
-        for (
-          let i = 1;
-          i <=
-          Math.ceil(
-            primeraproducts.data.TotalItems / primeraproducts.data.TotalPage
-          );
-          i++
-        ) {
-            var getStockProducts = {
-                method: "get",
-                url:
-                "https://rest.contabilium.com/api/inventarios/getStockByDeposito?id=28277" +
-                "&page=" +
-                i,
-                headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-                Authorization: `Bearer qf-h2yn_Y0tgZ4NEWDHi62VdoOT-8ZmqVx-lt3Kd6bstnWavOXa2GhujTZSvGHrwmJJbt1TWitqV8dOOurujoDhIhi_GONzQVHkkUe7hpV9SMP_XImCS3jympISn4PdkpeNDY6FC_bsBVSY7tZqajOGUCigUx6Jro9V0kUMyXcxVVZ7zIIWNGv3zhexha374XmBLBrRzWycRaC-pbMQAXSgrbVYWgCTSMBbLiw510ycHgdgBbnh7yF6WbWDABOtacBWJVdMfgHEKqHYwfxQcPGXLHtvHEJhrmO1QDSmXNLu3TbES5T9JcWTtkW82Ub3rep5IQ2S9R_tuYPgc0gw8vNIysgcEZQbH55_4DPZ9UaEA217vWqEYOPg9cfT5OYeQZ4z7fouPsfy81F4O7LIVHwuWZbEqi0gQmB2jNZGbcm1M0XIGMy00XHUOzZLrC5whUHTUWjmt6hKXm95vZH3wUaU-4IzXYf3Jc9OoWqY1PCsBk5ujdwo_3WACrrxK3J554JN6vRK7iyFzXxauGr-1w8hnhNt77fGO7i9dxBMCoIVNaeNCDilNmzsOFVn1lJDnCDGvy3CDo2ZpBgDrgh0DnA`,
-                },
-            };
-            const products = await axios(getStockProducts);
-            if(i === 1) console.log("Productos ",products.data);
-            let arregloPorFila = products.data.Items.map((e) => {
-                return {
-                    'Id': e.Id,
-                    'Codigo': e.Codigo,
-                    "Stock Actual": e.StockActual,
-                    "Stock con Reservas": e.StockConReservas,
-                }
-                });
-            await sheet.addRows(arregloPorFila);
-        }
-      res.status(200).json({ message: "Sucess" });
+            for (
+              let i = 1;
+              i <=
+              Math.ceil(
+                primeraproducts.data.TotalItems / primeraproducts.data.TotalPage
+              );
+              i++
+            ) {
+                var getStockProducts = {
+                    method: "get",
+                    url:
+                    "https://rest.contabilium.com/api/inventarios/getStockByDeposito?id=28277" +
+                    "&page=" +
+                    i,
+                    headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                    Authorization: `Bearer qf-h2yn_Y0tgZ4NEWDHi62VdoOT-8ZmqVx-lt3Kd6bstnWavOXa2GhujTZSvGHrwmJJbt1TWitqV8dOOurujoDhIhi_GONzQVHkkUe7hpV9SMP_XImCS3jympISn4PdkpeNDY6FC_bsBVSY7tZqajOGUCigUx6Jro9V0kUMyXcxVVZ7zIIWNGv3zhexha374XmBLBrRzWycRaC-pbMQAXSgrbVYWgCTSMBbLiw510ycHgdgBbnh7yF6WbWDABOtacBWJVdMfgHEKqHYwfxQcPGXLHtvHEJhrmO1QDSmXNLu3TbES5T9JcWTtkW82Ub3rep5IQ2S9R_tuYPgc0gw8vNIysgcEZQbH55_4DPZ9UaEA217vWqEYOPg9cfT5OYeQZ4z7fouPsfy81F4O7LIVHwuWZbEqi0gQmB2jNZGbcm1M0XIGMy00XHUOzZLrC5whUHTUWjmt6hKXm95vZH3wUaU-4IzXYf3Jc9OoWqY1PCsBk5ujdwo_3WACrrxK3J554JN6vRK7iyFzXxauGr-1w8hnhNt77fGO7i9dxBMCoIVNaeNCDilNmzsOFVn1lJDnCDGvy3CDo2ZpBgDrgh0DnA`,
+                    },
+                };
+                const products = await axios(getStockProducts);
+                if(i === 1) console.log("Productos ",products.data);
+                let arregloPorFila = products.data.Items.map((e) => {
+                    return {
+                        'Id': e.Id,
+                        'Codigo': e.Codigo,
+                        "Stock Actual": e.StockActual,
+                        "Stock con Reservas": e.StockConReservas,
+                    }
+                    });
+                await sheet.addRows(arregloPorFila);
+            }
+            res.status(200).json({ message: "Sucess" });
     } else {
       res.status(200).json({ message: "Error en doc sheets" });
     }
